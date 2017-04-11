769cbb856ffcdd26d112540b6bd5ce7844de1de8
Adam Reiter <adr5240@gmail.com>
2017-04-11 15:01:42 -0400

    Fill out file structure

--- app/youtubeApp.js
+++ app/youtubeApp.js
@@ -1,1 +1,14 @@
-angular.module('YouTubeApp', []);
+angular.module('YouTubeApp', [
+    'ui.router',
+    'videos'
+])
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('main', {
+                url: '',
+                abstract: true
+            })
+        ;
+        $urlRouterProvider.otherwise('/');
+    })
+;
